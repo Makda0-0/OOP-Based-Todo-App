a4e9b491754c2dbf4015e9628289f83dbef2ae53
Makda Solomon <makdas205@gmail.com>
2025-12-19 09:30:54 +0300

    Add initial tasks data structure

---
+++ tasks.py
@@ -0,0 +1,14 @@
+[
+    {
+        "id": 1,
+        "title": "wash car",
+        "description": "wasing car",
+        "completed": true
+    },
+    {
+        "id": 2,
+        "title": "feed dog",
+        "description": "",
+        "completed": false
+    }
+]
